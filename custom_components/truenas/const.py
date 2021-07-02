8da39329c5f25299de3041baa33dc53696724521
Shawn Wilsher <656602+sdwilsh@users.noreply.github.com>
2021-07-01 19:31:44 -0700

    Add support for jail running status as well as service to start/stop/restart

--- custom_components/truenas/const.py
+++ custom_components/truenas/const.py
@@ -10,6 +10,15 @@ CONF_AUTH_API_KEY = "API Key"
 
 DEFAULT_SCAN_INTERVAL_SECONDS = 30
 
+SERVICE_JAIL_START = "jail_start"
+SCHEMA_SERVICE_JAIL_START = {}
+SERVICE_JAIL_STOP = "jail_stop"
+SCHEMA_SERVICE_JAIL_STOP = {
+    vol.Optional("force"): cv.boolean,
+}
+SERVICE_JAIL_RESTART = "jail_restart"
+SCHEMA_SERVICE_JAIL_RESTART = {}
+
 SERVICE_VM_START = "vm_start"
 SCHEMA_SERVICE_VM_START = {
     vol.Optional("overcommit"): cv.boolean,
